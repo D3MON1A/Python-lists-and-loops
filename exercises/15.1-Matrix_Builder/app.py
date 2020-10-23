d48b78f319b3d68abed14cbdadbe63eac4ef133d
leisyarcia <leisyarcia@gmail.com>
2020-10-23 20:55:46 +0000

    Matrix fixed

--- exercises/15.1-Matrix_Builder/app.py
+++ exercises/15.1-Matrix_Builder/app.py
@@ -1,24 +1,28 @@
 import random
 
-def matrixBuilder(i):
-    numb_of_matrix = i
-    # matrix = []
-    matrix = [1] * numb_of_matrix
-    for i in range(numb_of_matrix):
-        matrix[i] = [1] * numb_of_matrix 
+# def matrixBuilder(i):
+#     numb_of_matrix = i
+#     # matrix = []
+#     matrix = [1] * numb_of_matrix
+#     for i in range(numb_of_matrix):
+#         matrix[i] = [1] * numb_of_matrix 
         
-    print(matrix)
+#     print(matrix)
 
 
-matrixBuilder(5)
+# matrixBuilder(5)
 # import random
 # #Create the function below:
 # matrix = []
 # def matrixBuilder(param):
 #   my_list = []
 #   for i in range(0, param, 1):
-#     my_list.append(random.randint(0,1))
+#     my_list.append(random.randint(1,1))
 #     for x in range(0, param, 1):
 #       matrix.append(my_list)
 #   return matrix
 # print(matrixBuilder(5)) 
+def matrixBuilder(numb):
+    new_list =[[1 for i in range(numb)] for x in range (numb)]
+    return new_list
+print(matrixBuilder(5))
--- exercises/16-Techno_beat/app.py
+++ exercises/16-Techno_beat/app.py
@@ -1,14 +1,14 @@
 
-def lyrics_generator(ones_and_zeros_list):
-  the_song = ""
-  for x in range(len(the_song)):
-    if the_song[x] == 0:
-      x += 'Boom '
-    elif the_song[x] == 1:
-      x += 'Drop the base '
-      if the_song[x-1] == 1 and the_song[x-2] == 1:
-        x += '!!!Break the base!!! '
-  return x
+def lyrics_generator(my_list):
+  string = ""
+  for x in range(len(my_list)):
+    if my_list[x] == 0:
+      string += "Boom "
+    if my_list[x] == 1:
+      string += "Drop the base "
+      if my_list[x-1] == 1 and my_list[x-2] == 1 :
+        string += "!!!Break the base!!!" 
+  return string
 # Your code go above, nothing to change after this line:
 print(lyrics_generator([0,0,1,1,0,0,0]))
 print(lyrics_generator([0,0,1,1,1,0,0,0]))
