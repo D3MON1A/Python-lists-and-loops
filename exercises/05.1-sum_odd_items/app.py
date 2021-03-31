ffa43042f6cd91a16b0e392b3e35c320f9463343
leisyarcia <leisyarcia@gmail.com>
2021-03-31 03:17:01 +0000

    pr

--- exercises/05.1-sum_odd_items/app.py
+++ exercises/05.1-sum_odd_items/app.py
@@ -1,10 +1,10 @@
 arr = [4,5,734,43,45,100,4,56,23,67,23,58,45]
 
 #Your code go here:
-def sumOdds():
+def sumOdds(number):
  totalSum=0
- for number in range(0,len(arr)):
-     if (arr[number] % 2 !=0):
+ for  in range(0,len(arr)):
+     if (arr[number] % 2) !=0:
         totalSum=totalSum + arr[number]
      print (totalSum)
 sumOdds()
